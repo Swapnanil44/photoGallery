22a9d1dd7a60b3cc5514d58ac71e5ae5c5d2ef3b
Swapnanil <adioticswapnanil@gmail.com>
2024-11-17 13:10:10 +0530

    updated ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"
 
-export default async function HomePage() {
+async function Images(){
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id)
   });
   //console.log(images);
   return (
-    <main className="">
       <div className="flex flex-wrap gap-4 w-full justify-center">
-        
         {[...images,...images,...images].map((image)=>(
           <div key={image.id * Math.random()} className="w-96 flex flex-col">
             <img src={image.url} alt="" />
@@ -19,9 +18,26 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
-      <div>
-        
-      </div>
-    </main>
   );
+}
+
+function LoggedOutText(){
+  return (
+    <div className="flex flex-col justify-center text-center min-h-screen text-5xl font-bold">
+      Please Sign Up 
+    </div>
+  )
+}
+
+export default async function HomePage() {
+  return (
+    <main>
+      <SignedOut>
+        <LoggedOutText/>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
+    </main>
+  )
 }
