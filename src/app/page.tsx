283ad48cd492b0bc0e08237ab80a737a47beb3bc
Swapnanil <adioticswapnanil@gmail.com>
2024-11-17 12:08:50 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic"
 
 const mockUrls = [
   "https://utfs.io/f/v68bdIJZrOQ5PGP4O7FSqVRwbafOTolp9Dm3hzJ8u6x2ePjG",
@@ -16,11 +17,16 @@ const mockImages = mockUrls.map((url,index)=>(
  }));
 
 export default async function HomePage() {
-  const post = await db.query.posts.findMany();
-  console.log(post);
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4 w-full justify-center">
+        {posts.map((post)=>(
+          <div key={post.id} className="text-2xl" >
+             {post.name}
+          </div>
+        ))}
         {[...mockImages,...mockImages,...mockImages].map((image)=>(
           <div key={Math.random()} className="w-96">
             <img src={image.url} alt="" />
